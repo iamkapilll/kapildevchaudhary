bc9a5cfb16e21e01d0da109a00f58659fa203d2c
Kapil Dev Chaudhary <kapildevchaudhary067@gmail.com>
2026-05-13 01:59:41 +0545

    wrapped everyting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,17 @@ import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import Footer from "@/components/Footer";
+import SkillsSection from "@/components/SkillsSection";
+import ContactSection from "@/components/ContactSection";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
+      <SkillsSection />
       <AboutSection />
       <ProjectsSection />
+      <ContactSection />
       <Footer />
       {/* Add other sections here */}
     </>
